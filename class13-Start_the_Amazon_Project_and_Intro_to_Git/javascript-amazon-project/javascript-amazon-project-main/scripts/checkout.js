f6b74cdcd907245a5207c0d4af671961f71abd95
Guilherme Cotrim <guisobiso@gmail.com>
2025-05-15 14:23:37 -0300

    exercise 14f

--- class13-Start_the_Amazon_Project_and_Intro_to_Git/javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
+++ class13-Start_the_Amazon_Project_and_Intro_to_Git/javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
@@ -34,7 +34,7 @@ cart.forEach((cartItem) => {
             <span>
               Quantity: <span class="quantity-label js-cart-quantity">${cartItem.quantity}</span>
             </span>
-            <span class="update-quantity-link link-primary js-update-link">
+            <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
               Update
             </span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
@@ -118,6 +118,7 @@ document.querySelectorAll('.js-delete-link')
 document.querySelectorAll('.js-update-link')
   .forEach((link) => {
     link.addEventListener('click', () => {
-      
+      const productId = link.dataset.productId;
+      console.log(productId);
     });
   });
