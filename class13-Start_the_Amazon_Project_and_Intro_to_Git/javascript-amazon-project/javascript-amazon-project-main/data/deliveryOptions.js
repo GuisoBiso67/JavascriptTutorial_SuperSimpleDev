2def59bc44eee33ea3fc12f9c07b458f9e1b6e0b
Guilherme Cotrim <guisobiso@gmail.com>
2025-05-17 14:17:32 -0300

    Save data and generate HTML for delivery options (class 15 part 1)

---
+++ class13-Start_the_Amazon_Project_and_Intro_to_Git/javascript-amazon-project/javascript-amazon-project-main/data/deliveryOptions.js
@@ -0,0 +1,13 @@
+export const deliveryOptions = [{
+  id: 'i1',
+  deliveryDays: 7,
+  priceCents: 0,
+}, {
+  id: 'i2',
+  deliveryDays: 3,
+  priceCents: 499,
+}, {
+  id: 'i3',
+  deliveryDays: 1,
+  priceCents: 999,
+}];
