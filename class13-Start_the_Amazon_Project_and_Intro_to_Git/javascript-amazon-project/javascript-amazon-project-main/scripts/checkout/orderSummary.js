e51fa2009dcbebe32da8c468225e67d883c2dce6
Guilherme Cotrim <guisobiso@gmail.com>
2025-05-18 13:00:35 -0300

    exercises 15j and 15k (Generate header with js)

--- class13-Start_the_Amazon_Project_and_Intro_to_Git/javascript-amazon-project/javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ class13-Start_the_Amazon_Project_and_Intro_to_Git/javascript-amazon-project/javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -5,6 +5,7 @@ import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'; // default export
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
+import { renderCheckoutHeader } from './checkoutHeader.js';
 
 export function renderOrderSummary(){
   let cartSummaryHTML = '';
@@ -112,11 +113,7 @@ export function renderOrderSummary(){
 
   document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
-  function updateCartQuantity(){
-    document.querySelector('.js-return-to-home-link').innerHTML = `${calculateCartQuantity()} items`;
-  };
-
-  updateCartQuantity();
+  renderCheckoutHeader();
 
   document.querySelectorAll('.js-delete-link')
     .forEach((link) => {
@@ -126,7 +123,7 @@ export function renderOrderSummary(){
 
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         container.remove();
-        updateCartQuantity();
+        renderCheckoutHeader();
 
         renderPaymentSummary();
         renderOrderSummary();
@@ -158,7 +155,7 @@ export function renderOrderSummary(){
 
           const container = document.querySelector(`.js-cart-item-container-${productId}`);
           container.remove();
-          updateCartQuantity();
+          renderCheckoutHeader();
 
           renderPaymentSummary();
           renderOrderSummary();
