40a669bc9b6923e90243af5ad5ae469cf65352be
Guilherme Cotrim <guisobiso@gmail.com>
2025-05-24 10:36:51 -0300

    exercises 16f 16g 16h

--- class16B-Testing_Framework/tests/checkout/orderSummaryTest.js
+++ class16B-Testing_Framework/tests/checkout/orderSummaryTest.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
+import {formatCurrency} from "../../scripts/utils/money.js"
 
 describe('test suite: renderOrderSummary', () => {
 
@@ -35,7 +36,10 @@ describe('test suite: renderOrderSummary', () => {
     expect(document.querySelector(`.js-product-quantity-${productId1}`).innerText).toContain('Quantity: 2');
     expect(document.querySelector(`.js-product-quantity-${productId2}`).innerText).toContain('Quantity: 1');
 
-    document.querySelector('.js-test-container').innerHTML = '';
+    expect(document.querySelector(`.js-product-name-${productId1}`).innerText).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
+    expect(document.querySelector(`.js-product-name-${productId2}`).innerText).toEqual('Intermediate Size Basketball');
+    expect(document.querySelector(`.js-product-price-${productId1}`).innerText).toEqual(`$${formatCurrency(1090)}`);
+    expect(document.querySelector(`.js-product-price-${productId2}`).innerText).toEqual(`$${formatCurrency(2095)}`);
   });
 
   it('removes a product', () => {
@@ -44,9 +48,14 @@ describe('test suite: renderOrderSummary', () => {
     expect(document.querySelector(`.js-cart-item-container-${productId1}`)).toEqual(null);
     expect(document.querySelector(`.js-cart-item-container-${productId2}`)).not.toEqual(null);
 
+    expect(document.querySelector(`.js-product-name-${productId2}`).innerText).toEqual('Intermediate Size Basketball'); // first product was deleted
+    expect(document.querySelector(`.js-product-price-${productId2}`).innerText).toEqual(`$${formatCurrency(2095)}`);
+
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
+  });
 
-    document.querySelector('.js-test-container').innerHTML = '';    
-    });
+  afterEach(() => {
+    document.querySelector('.js-test-container').innerHTML = '';
+  })
 });
