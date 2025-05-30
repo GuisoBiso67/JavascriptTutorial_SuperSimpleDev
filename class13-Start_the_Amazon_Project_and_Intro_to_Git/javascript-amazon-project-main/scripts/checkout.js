7f9e7f677234b042adf85b6eb2309ef382ff0d50
Guilherme Cotrim <guisobiso@gmail.com>
2025-05-30 17:32:51 -0300

    Convert products into classes

--- class13-Start_the_Amazon_Project_and_Intro_to_Git/javascript-amazon-project-main/scripts/checkout.js
+++ class13-Start_the_Amazon_Project_and_Intro_to_Git/javascript-amazon-project-main/scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import '../data/cart-class.js'; // runs all the code inside a file;
+//import '../data/cart-class.js'; // runs all the code inside a file;
 
 renderCheckoutHeader();
 renderOrderSummary();
