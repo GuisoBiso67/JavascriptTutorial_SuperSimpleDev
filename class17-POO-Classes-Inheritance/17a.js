f4b41208875ddc03051a8044389994ebd8f66b72
Guilherme Cotrim <guisobiso@gmail.com>
2025-05-31 12:37:21 -0300

    17e and 17f

--- class17-POO-Classes-Inheritance/17a.js
+++ class17-POO-Classes-Inheritance/17a.js
@@ -1,48 +1,91 @@
 class Car {
-  brand;
-  model;
+  #brand;
+  #model;
   speed;
 
   constructor(cars){
-    this.brand = cars.brand;
-    this.model = cars.model;
+    this.#brand = cars.brand;
+    this.#model = cars.model;
     this.speed = cars.speed;
+    this.isTrunkOpen = cars.isTrunkOpen;
+    this.acceleration = cars.acceleration;
     //this.displayInfo();
   }
 
   displayInfo(){
-    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`);
+    console.log(`${this.#brand} ${this.#model}, Speed: ${this.speed} km/h, is Trunk Open? : ${this.isTrunkOpen}`);
   }
 
   go(){
-    this.speed += 5;
+    if(this.acceleration){
+      this.speed += this.acceleration;
+    }else{
+      if(this.speed === 200){
+        return;
+      }else{
+        this.speed += 5;
+    }
+    }
   }
 
   brake(){
     this.speed -= 5;
   }
-}
 
-const cars = [
-  {
-    brand: 'Toyota',
-    model: 'Corolla',
-    speed: 0,
-  },
-  {
-    brand: 'Tesla',
-    model: 'Model 3',
-    speed: 0,
+  openTrunk(){
+    if(this.acceleration){
+      return console.log(`${this.brand} do not have a trunk`);
+    }
+
+    if(this.speed > 0){
+      return;
+    }else{
+      this.isTrunkOpen = true;
+    }
   }
-].map((carDetails) => {
-  return new Car(carDetails);
-});
 
-const car1 = new Car(cars[0]);
-const car2 = new Car(cars[1]);
-
-for (let i=0;i<3;i++){
-  car1.go(); // speed 15km;
+  closeTrunk(){
+    if(this.acceleration){
+      return console.log(`${this.brand} do not have a trunk`);
+    }
+    this.isTrunkOpen = false;
+  }
 }
-car1.brake(); // speed 10km;
+
+class RaceCar extends Car{
+  acceleration = 0;
+
+  constructor(cars){
+    super(cars);
+    this.acceleration = cars.acceleration;
+  }
+}
+
+const car1 = new Car({
+  brand: 'Toyota',
+  model: 'Corolla',
+  speed: 0,
+  isTrunkOpen: false,
+});
+const car2 = new Car({
+  brand: 'Tesla',
+  model: 'Model 3',
+  speed: 0,
+  isTrunkOpen: false,
+});
+const car3 = new RaceCar({
+  brand: 'McLaren',
+  model: 'F1',
+  speed: 0,
+  acceleration: 20,
+})
+
+car1.openTrunk();
+car1.closeTrunk();
+car1.go();
+car1.openTrunk()
 car1.displayInfo();
+
+car3.go();
+car3.openTrunk();
+car3.displayInfo();
