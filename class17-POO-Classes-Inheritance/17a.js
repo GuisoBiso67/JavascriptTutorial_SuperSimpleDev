a3ae8ffd03777fb5fb4407659c5a450ba8a64b4d
Guilherme Cotrim <guisobiso@gmail.com>
2025-05-31 10:47:55 -0300

    17c

--- class17-POO-Classes-Inheritance/17a.js
+++ class17-POO-Classes-Inheritance/17a.js
@@ -1,15 +1,25 @@
 class Car {
   brand;
   model;
+  speed;
 
   constructor(cars){
     this.brand = cars.brand;
     this.model = cars.model;
+    this.speed = cars.speed;
     //this.displayInfo();
   }
 
   displayInfo(){
-    console.log(`${this.brand} ${this.model}`);
+    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`);
+  }
+
+  go(){
+    this.speed += 5;
+  }
+
+  brake(){
+    this.speed -= 5;
   }
 }
 
@@ -17,10 +27,12 @@ const cars = [
   {
     brand: 'Toyota',
     model: 'Corolla',
+    speed: 0,
   },
   {
     brand: 'Tesla',
     model: 'Model 3',
+    speed: 0,
   }
 ].map((carDetails) => {
   return new Car(carDetails);
@@ -28,5 +40,9 @@ const cars = [
 
 const car1 = new Car(cars[0]);
 const car2 = new Car(cars[1]);
+
+for (let i=0;i<3;i++){
+  car1.go(); // speed 15km;
+}
+car1.brake(); // speed 10km;
 car1.displayInfo();
-car2.displayInfo();
