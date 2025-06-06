59e4da85cf6f10b6443e43098a78648e469069da
Guilherme Cotrim <guisobiso@gmail.com>
2025-06-06 15:48:40 -0300

    Learn error handling

--- class13-Start_the_Amazon_Project_and_Intro_to_Git/javascript-amazon-project-main/scripts/checkout.js
+++ class13-Start_the_Amazon_Project_and_Intro_to_Git/javascript-amazon-project-main/scripts/checkout.js
@@ -7,15 +7,22 @@ import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
 async function loadPage(){ // async make a function return a promise;
-  console.log('load page');
+  try {
+    //throw 'error1'; // throw creates a error;
 
-  await loadProductsFetch(); // await lets us write asynchronous code like normal code, so we don't need to use 'then'. Await con only be used inside an async, if another function is inside async, we can't use await inside this other function;
+    await loadProductsFetch(); // await lets us write asynchronous code like normal code, so we don't need to use 'then'. Await con only be used inside an async, if another function is inside async, we can't use await inside this other function;
 
-  const value = await new Promise((resolve) => {
-    loadCart(() => {
-      resolve('value3');
+    const value = await new Promise((resolve, reject) => { // 'reject' allow us to create an error in the future;
+      //throw 'error2';
+      loadCart(() => {
+        //reject('error3');
+        resolve('value3');
+      });
     });
-  }) 
+  } catch (error) {
+    console.log('Unexpected Error. Please try again later.');
+  };
+ 
 
   renderCheckoutHeader();
   renderOrderSummary();
