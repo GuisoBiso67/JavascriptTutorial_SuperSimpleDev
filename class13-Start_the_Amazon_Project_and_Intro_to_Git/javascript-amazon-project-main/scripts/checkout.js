a4c226c47fdacada3b3889bfca14a1657fa1c91a
Guilherme Cotrim <guisobiso@gmail.com>
2025-06-06 15:22:17 -0300

    Convert from promises to async await

--- class13-Start_the_Amazon_Project_and_Intro_to_Git/javascript-amazon-project-main/scripts/checkout.js
+++ class13-Start_the_Amazon_Project_and_Intro_to_Git/javascript-amazon-project-main/scripts/checkout.js
@@ -6,6 +6,26 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
+async function loadPage(){ // async make a function return a promise;
+  console.log('load page');
+
+  await loadProductsFetch(); // await lets us write asynchronous code like normal code, so we don't need to use 'then'. Await con only be used inside an async, if another function is inside async, we can't use await inside this other function;
+
+  const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve('value3');
+    });
+  }) 
+
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+
+  //return 'value2'; // equals to resolve('value2');
+}
+loadPage();
+
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -19,6 +39,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 /*
 // use promises instead of callbacks. Promises keep our code more flat;
