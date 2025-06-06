ef9b32ecdd33cb6ae02ee527efcc4e0e5662507b
Guilherme Cotrim <guisobiso@gmail.com>
2025-06-06 16:16:35 -0300

    create order using the backend

--- class13-Start_the_Amazon_Project_and_Intro_to_Git/javascript-amazon-project-main/scripts/checkout/paymentSummary.js
+++ class13-Start_the_Amazon_Project_and_Intro_to_Git/javascript-amazon-project-main/scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart, calculateCartQuantity} from "../../data/cart.js";
 import { getProduct, loadProducts} from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import formatCurrency from '../utils/money.js';
+import {addOrder} from '../../data/orders.js';
 
 loadProducts(renderPaymentSummary);
 
@@ -53,10 +54,34 @@ export function renderPaymentSummary(){
       <div class="payment-summary-money">$${formatCurrency(totalAfterTax)}</div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary js-place-order">
       Place your order
     </button>
   `;
 
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+  // DID NOT WORK: {errorMessage: 'Error: something went wrong on the backend. Please refresh and try again.'}
+  // looks like backend is not working, I tried several times!
+  document.querySelector('.js-place-order').addEventListener('click', async () => {
+    try{
+      const response = await fetch('https://supersimplebackend.dev/orders', { 
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
+
+      const order = await response.json();
+      addOrder(order);
+    } catch(error){
+      console.log('Unexpected Error. MY ERROR');
+    }
+
+    window.location.href = 'orders.html';
+  });
 }
+// -----------------------------------------------------------------------------
